183a15c6db6f9713b10710d7010f410639f600f1
naimurcse <2015naim@gmail.com>
2022-06-03 21:22:29 +0600

    load product by API

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,22 @@ const loadData = () => {
 
 const displayProducts = products => {
     // console.log(products);
+    let productCards = document.getElementById("product-cards");
     products.forEach(product => {
         console.log(product);
+        console.log(product.brand);
+        console.log(product.phone_name);
+        console.log(product.image);
+        console.log(product.slug);
+        const productCard = document.createElement("div");
+        productCard.classList.add("product-card");
+        productCard.innerHTML = `
+            <img src="${product.image}" alt="" />
+            <h3>${product.phone_name}</h3>
+            <h5>${product.brand}</h5>
+            <button>Explore</button>
+        `
+
+        productCards.appendChild(productCard);
     });
 }
